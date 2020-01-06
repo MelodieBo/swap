a1eb2c9a4f19cd2143bff32d58c89ec0f8bc9c75
MelodieBouillot <bouillot.melodie@gmail.com>
2020-01-06 16:48:07 +0100

    migrations ok

--- src/Migrations/Version20200106154646.php
+++ src/Migrations/Version20200106154646.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200106150339 extends AbstractMigration
+final class Version20200106154646 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,6 @@ final class Version20200106150339 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE categorie (id INT AUTO_INCREMENT NOT NULL, femme VARCHAR(255) DEFAULT NULL, homme VARCHAR(255) DEFAULT NULL, enfant VARCHAR(255) DEFAULT NULL, maison VARCHAR(255) DEFAULT NULL, media_loisir VARCHAR(255) DEFAULT NULL, autre VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE produit ADD categorie_id INT NOT NULL');
         $this->addSql('ALTER TABLE produit ADD CONSTRAINT FK_29A5EC27BCF5E72D FOREIGN KEY (categorie_id) REFERENCES categorie (id)');
         $this->addSql('CREATE INDEX IDX_29A5EC27BCF5E72D ON produit (categorie_id)');
@@ -34,7 +33,6 @@ final class Version20200106150339 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE produit DROP FOREIGN KEY FK_29A5EC27BCF5E72D');
-        $this->addSql('DROP TABLE categorie');
         $this->addSql('DROP INDEX IDX_29A5EC27BCF5E72D ON produit');
         $this->addSql('ALTER TABLE produit DROP categorie_id');
     }
