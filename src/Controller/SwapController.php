b72673943f25f89bcdba91bf563530fc94b8fa7c
ngvinhanh <ngvinhanh@gmail.com>
2020-01-07 11:29:24 +0100

    Créer 2 Controller Swap et Security

---
+++ src/Controller/SwapController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SwapController extends AbstractController
+{
+    /**
+     * @Route("/swap", name="swap")
+     */
+    public function index()
+    {
+        return $this->render('swap/index.html.twig', [
+            'controller_name' => 'SwapController',
+        ]);
+    }
+}
