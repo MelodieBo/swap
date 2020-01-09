e00088e425cf966a2744ce8bac6e5b093797f492
MelodieBouillot <bouillot.melodie@gmail.com>
2020-01-09 15:32:40 +0100

    test fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,27 +4,27 @@ namespace App\DataFixtures;
 
 use App\Entity\Produit;
 use App\Entity\Categorie;
+use App\DataFixtures\AppFixtures;
 use App\Repository\CategorieRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager, CategorieRepository $repo)
+    public function load(ObjectManager $manager)
     {
-        
-        for ($i = 1; $i < 30; $i++) {
-
-            $rand = array_rand($repo, 1);
-
-            $produit = new Produit;
-            $produit->setCategorie($rand)
-                    ->setUser()
-                    ->setTitre("Cadeau n° $i")
-                    ->setDescription("Descrpition optionnelle du cadeau n° $i")
-                    ->setValeur("Prix du cadeau n° $i" . mt_rand(10, 100));
-
-            $manager->persist($produit);
+        // create 20 products! Bam!
+        dd($repo);
+        for ($i = 0; $i < 30; $i++) {
+            
+            $product = new Produit();
+            $product->setTitre('')
+                    ->setDescription('')
+                    ->setValeur('')
+                    ->setUser('')
+                    ->setImage('')
+                    ->setCreeLe('')
+                    ->setCategorie('');
         }
 
         $manager->flush();
