08d977d8cfeb411130e2b2640ba1c57cac07dfd1
MelodieBouillot <bouillot.melodie@gmail.com>
2020-01-10 09:56:42 +0100

    test fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -13,17 +13,17 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // create 20 products! Bam!
+
         dd($repo);
         for ($i = 0; $i < 30; $i++) {
             
             $product = new Produit();
-            $product->setTitre('')
-                    ->setDescription('')
+            $product->setTitre('Lorem ipsum dolor sit amet')
+                    ->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit. In iaculis lacus non lacus pharetra iaculis. Praesent dui quam, molestie a porta ut, fringilla quis neque. Vivamus vestibulum lectus laoreet ex condimentum ultricies. Fusce luctus elit sed eleifend feugiat. Nullam eu felis eget neque porttitor pellentesque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec blandit nunc in maximus fringilla. Maecenas quis massa dictum, varius enim fermentum, placerat enim.')
                     ->setValeur('')
                     ->setUser('')
-                    ->setImage('')
-                    ->setCreeLe('')
+                    ->setImage('https://cdn.pixabay.com/photo/2014/05/03/00/50/video-controller-336657_1280.jpg')
+                    ->setCreeLe(new \DATETIME())
                     ->setCategorie('');
         }
 
