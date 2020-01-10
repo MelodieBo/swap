b3d0fdbade79d23fd6bb849bdb1ca1de6b952a53
MelodieBouillot <bouillot.melodie@gmail.com>
2020-01-10 16:50:31 +0100

    faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,29 +2,69 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use App\Entity\Produit;
 use App\Entity\Categorie;
 use App\DataFixtures\AppFixtures;
-use App\Repository\CategorieRepository;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
+
 {
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-
-        dd($repo);
-        for ($i = 0; $i < 30; $i++) {
+        $users = [];
+        // fixtures pour users
+        $faker = \Faker\Factory::create('fr_FR');
+        for ($i=0; $i < 10; $i++) { 
+            $user = new User();
+            $user->setEmail($faker->email())
+                 ->setPassword($faker->password($this->encoder->encodePassword($user,'toto')))
+                 ->setAdresse($faker->address)
+                 ->setTelephone($faker->phoneNumber)
+                 ->setVille($faker->country)
+                 ->setCodePostale($faker->postcode);
+            $manager->persist($user);
             
+            $users[] = $user;
+        }
+        $manager->flush();
+        //dd($user);
+        $categories = [
+            (new Categorie())->setLibelle('femme'),
+            (new Categorie())->setLibelle('homme'),
+            (new Categorie())->setLibelle('enfant'),
+            (new Categorie())->setLibelle('maison'),
+            (new Categorie())->setLibelle('media'),
+            (new Categorie())->setLibelle('autre'),
+        ];
+
+        for ($i=0; $i < count($categories); $i++) { 
+            $manager->persist($categories[$i]);
+        }
+        $manager->flush();
+        
+
+        for ($i = 0; $i < 30; $i++) {
+            $categorie = $categories[0];
+            $user = $users[0];
             $product = new Produit();
-            $product->setTitre('Lorem ipsum dolor sit amet')
-                    ->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit. In iaculis lacus non lacus pharetra iaculis. Praesent dui quam, molestie a porta ut, fringilla quis neque. Vivamus vestibulum lectus laoreet ex condimentum ultricies. Fusce luctus elit sed eleifend feugiat. Nullam eu felis eget neque porttitor pellentesque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec blandit nunc in maximus fringilla. Maecenas quis massa dictum, varius enim fermentum, placerat enim.')
-                    ->setValeur('')
-                    ->setUser('')
+            $product->setTitre($faker->realText())
+                    ->setDescription($faker->sentence())
+                    ->setValeur(13)
+                    ->setUser($user)
                     ->setImage('https://cdn.pixabay.com/photo/2014/05/03/00/50/video-controller-336657_1280.jpg')
                     ->setCreeLe(new \DATETIME())
-                    ->setCategorie('');
+                    ->setCategorie($categorie);
+                    
+            $manager->persist($product);
         }
 
         $manager->flush();
